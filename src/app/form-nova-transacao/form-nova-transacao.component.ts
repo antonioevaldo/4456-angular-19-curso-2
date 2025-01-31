576ab3d3470ae5aabf06349f5b8776e7ae2b5c6e
Antônio Evaldo <antonio.evaldo349@gmail.com>
2025-01-31 16:40:08 -0300

    Vídeo 3.4 - ngModel no select e validação nativa do HTML

--- src/app/form-nova-transacao/form-nova-transacao.component.ts
+++ src/app/form-nova-transacao/form-nova-transacao.component.ts
@@ -8,9 +8,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './form-nova-transacao.component.css'
 })
 export class FormNovaTransacaoComponent {
-  valorTransacao = "40";
+  tipoTransacao = "";
+  valorTransacao = "";
 
   aoSubmeter() {
+    console.log(this.tipoTransacao);
     console.log(this.valorTransacao);
   }
 }
